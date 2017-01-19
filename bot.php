f332deb416430335abd8effd2615d56fa83addb0
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-19 14:04:34 +0700

    no message

--- bot.php
+++ bot.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 require_once __DIR__ . "/vendor/autoload.php";
 
 use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 // Set these
 $config = [
@@ -14,7 +14,7 @@ $config = [
     'channelSecret' => '515995d49d4801e7c580b8c914709b35',
     'channelMid' => 'Ubb0233685f6c43ad7af9f72476d67f16',
 ];
-$sdk = new LINEBot($config, new GuzzleHTTPClient($config));
+$sdk = new LINEBot($config, new CurlHTTPClient($config));
 
 $postdata = @file_get_contents("php://input");
 $messages = $sdk->createReceivesFromJSON($postdata);
