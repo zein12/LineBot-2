ae8b644809d00b8de3a8dd58b0a05e0ec23d8095
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-19 14:11:29 +0700

    no message

--- bot.php
+++ bot.php
@@ -7,7 +7,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 
 use LINE\LINEBot;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-
+$access_token = 'wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=';
 // Set these
 $config = [
     'channelId' => '1496021296',
@@ -46,10 +46,27 @@ if($signature && $sdk->validateSignature($postdata, $signature)) {
             }
         }
     }else{
-		$result = $sdk->sendText(['555'], '555');
-		if(!$result instanceof LINE\LINEBot\Response\SucceededResponse) {
-			error_log('LINE error: ' . json_encode($result));
-		}
+		$messages['type'] = 'text';
+		$messages['text'] = 'Hello';
+		$url = 'https://api.line.me/v2/bot/message/reply';
+			
+		$data = [
+			'replyToken' => $replyToken,
+			'messages' => [$messages],
+		];
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		
+		echo $result . '\r\n';
 	}
 } else {
     error_log('LINE signatures didn\'t match');
