7b08184552b037a8a1d88f93ad9031bdffeba4a9
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-19 16:16:10 +0700

    no message

--- bot.php
+++ bot.php
@@ -4,6 +4,7 @@ require_once $path;
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '515995d49d4801e7c580b8c914709b35']);
+$mid = 'Ubb0233685f6c43ad7af9f72476d67f16';
 
 $postdata = file_get_contents("php://input");
 // Parse JSON
@@ -12,9 +13,9 @@ $events = json_decode($postdata, true);
 if(!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			$text = $event['message']->getText();
+			$text = $event['message']['text'];
 			$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
-			$response = $bot->pushMessage('Ubb0233685f6c43ad7af9f72476d67f16', $textMessageBuilder);
+			$response = $bot->pushMessage($mid, $textMessageBuilder);
 		}
 	}
 	
