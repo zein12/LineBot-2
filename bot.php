0a826a086465aea99923f111cae113d76cd9689a
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-19 10:53:14 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,33 +1,56 @@
 <?php
-require_once('./vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+
+error_reporting(E_ALL);
+
+// SDK is installed via composer
+require_once __DIR__ . "/vendor/autoload.php";
+
 use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient;
-use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
-$httpClient = new CurlHTTPClient('wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=');
+// Set these
+$config = [
+    'channelId' => 'wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=',
+    'channelSecret' => '515995d49d4801e7c580b8c914709b35',
+];
+$sdk = new LINEBot($config, new CurlHTTPClient($config));
 
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	
-	$httpClient = new CurlHTTPClient('wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=');
-	$bot = new LINEBot($httpClient, ['channelSecret' => '515995d49d4801e7c580b8c914709b35']);
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		if ($event['type'] == 'message'){
-			
-			$replyToken = $event['replyToken'];
-			
-			$textMessageBuilder = new TextMessageBuilder('hello');
-			$response = $bot->replyMessage($replyToken, $textMessageBuilder);
-			if ($response->isSucceeded()) {
-				echo 'Succeeded!';
-				return;
-			}
-		}
-	}
+$postdata = @file_get_contents("php://input");
+$messages = $sdk->createReceivesFromJSON($postdata);
+
+// Verify the signature
+// REF: http://line.github.io/line-bot-api-doc/en/api/callback/post.html#signature-verification
+$sigheader = 'LineBot';
+// REF: http://stackoverflow.com/a/541450
+$signature = @$_SERVER[ 'HTTP_'.strtoupper(str_replace('-','_',$sigheader)) ];
+if($signature && $sdk->validateSignature($postdata, $signature)) {
+    // Next, extract the messages
+    if(is_array($messages)) {
+        foreach ($messages as $message) {
+            if ($message instanceof LINEBot\Receive\Message\Text) {
+                $text = $message->getText();
+                if ($text == "mid") {
+                    $fromMid = $message->getFromMid();
+
+                    // Send the mid back to the sender and check if the message was delivered
+                    $result = $sdk->sendText([$fromMid], 'mid: ' . $fromMid);
+                    if(!$result instanceof LINE\LINEBot\Response\SucceededResponse) {
+                        error_log('LINE error: ' . json_encode($result));
+                    }
+                } else {
+                    $result = $sdk->sendText('555');
+                    if(!$result instanceof LINE\LINEBot\Response\SucceededResponse) {
+                        error_log('LINE error: ' . json_encode($result));
+                    }
+                }
+            } else {
+                $result = $sdk->sendText('666');
+				if(!$result instanceof LINE\LINEBot\Response\SucceededResponse) {
+					error_log('LINE error: ' . json_encode($result));
+				}
+            }
+        }
+    } // Else, error
+} else {
+    error_log('LINE signatures didn\'t match');
 }
-echo 'OK';
