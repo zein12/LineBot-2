2845775c384ed921b89eec22d356903b1d1dd63b
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-19 17:02:54 +0700

    no message

--- bot.php
+++ bot.php
@@ -30,7 +30,7 @@ if(!is_null($events['events'])) {
 				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\AudioMessageBuilder('https://example.com/original.m4a',240000);
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}else if(strpos($text, 'location') !== false){
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder('','',35.65910807942215,139.70372892916203);
+				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder('','〒150-0002 東京都渋谷区渋谷２丁目２１−１',35.65910807942215,139.70372892916203);
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}else if(strpos($text, 'sticker') !== false){
 				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1,1);
