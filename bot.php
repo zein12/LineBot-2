f800ad8c020cb2a46d1b99966f5bcde71678d66e
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-19 13:58:35 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,118 +1,51 @@
 <?php
-$access_token = 'wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=';
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['message']['text'];
-			$id = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-			
-			// Build message to reply back
-			if(strpos($text, 'h') !== false){
-				$messages['type'] = 'text';
-				$messages['text'] = $id;
-			}else if(strpos($text, 'z') !== false){
-				$messages[0]['type'] = 'text';
-				$messages[0]['text'] = 'Hello';
-				$messages[1]['type'] = 'text';
-				$messages[1]['text'] = 'Hi';
-			}else if($text == 'image' || $text == 'imag' || $text == 'img' || $text == 'png' || $text == 'jpg'){
-				$messages = [
-					'type' => 'image',
-					'originalContentUrl' => 'https://upload.wikimedia.org/wikipedia/commons/b/b4/JPEG_example_JPG_RIP_100.jpg',
-					'previewImageUrl' => 'https://upload.wikimedia.org/wikipedia/en/6/6d/Pullinger-150x150.jpg'
-				];
-			}else if($text == 'video' || $text == 'vid'){
-				$messages = [
-					'type' => 'video',
-					'originalContentUrl' => 'https://example.com/original.mp4',
-					'previewImageUrl' => 'https://upload.wikimedia.org/wikipedia/en/6/6d/Pullinger-150x150.jpg'
-				];
-			}else if($text == 'audio'){
-				$messages = [
-					'type' => 'audio',
-					'originalContentUrl' => 'https://example.com/original.m4a',
-					'duration' => 240000
-				];
-			}else if($text == 'location'){
-				$messages = [
-					'type' => 'location',
-					'title' => 'my location',
-					'address' => '〒150-0002 東京都渋谷区渋谷２丁目２１−１',
-					'latitude' => 35.65910807942215,
-					'longitude' => 139.70372892916203
-				];
-			}else if($text == 'sticker'){
-				$messages = [
-					'type' => 'sticker',
-					'packageId' => '1',
-					'stickerId' => '1'
-				];
-			}else{
-				$messages = [
-					'type' => 'text',
-					'text' => '555'
-				];
-			}
-			
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+// Show all errors for testing
+error_reporting(E_ALL);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+// SDK is installed via composer
+require_once __DIR__ . "/includes/vendor/autoload.php";
 
-			echo $result . '\r\n';
-		}
-		if ($event['type'] == 'message' && $event['message']['type'] == 'sticker') {
-			$replyToken = $event['replyToken'];
-			$packageId = $event['message']['packageId'];
-			$stickerId =  $event['message']['stickerId'];
-			
-			$messages = [
-				'type' => 'sticker',
-				'packageId' => $packageId,
-				'stickerId' => $stickerId
-			];
-			
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-		}
-	}
+// Set these
+$config = [
+    'channelId' => '1496021296',
+    'channelSecret' => '515995d49d4801e7c580b8c914709b35',
+    'channelMid' => 'Ubb0233685f6c43ad7af9f72476d67f16',
+];
+$sdk = new LINEBot($config, new GuzzleHTTPClient($config));
+
+$postdata = @file_get_contents("php://input");
+$messages = $sdk->createReceivesFromJSON($postdata);
+
+// Verify the signature
+// REF: http://line.github.io/line-bot-api-doc/en/api/callback/post.html#signature-verification
+$sigheader = 'X-LINE-ChannelSignature';
+// REF: http://stackoverflow.com/a/541450
+$signature = @$_SERVER[ 'HTTP_'.strtoupper(str_replace('-','_',$sigheader)) ];
+if($signature && $sdk->validateSignature($postdata, $signature)) {
+    // Next, extract the messages
+    if(is_array($messages)) {
+        foreach ($messages as $message) {
+            if ($message instanceof LINEBot\Receive\Message\Text) {
+                $text = $message->getText();
+                if ($text == "mid") {
+                    $fromMid = $message->getFromMid();
+
+                    // Send the mid back to the sender and check if the message was delivered
+                    $result = $sdk->sendText([$fromMid], 'mid: ' . $fromMid);
+                    if(!$result instanceof LINE\LINEBot\Response\SucceededResponse) {
+                        error_log('LINE error: ' . json_encode($result));
+                    }
+                } else {
+                    // Process normally, or do nothing
+                }
+            } else {
+                // Process other types of LINE messages like image, video, sticker, etc.
+            }
+        }
+    } // Else, error
+} else {
+    error_log('LINE signatures didn\'t match');
 }
-echo 'OK';
