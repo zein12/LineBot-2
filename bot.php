cf8fa46e79777534c26c0bd3b426546fd3314551
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-20 11:51:37 +0700

    no message

--- bot.php
+++ bot.php
@@ -56,4 +56,6 @@ if(!is_null($events['events'])) {
 $Message1 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('Yes','yes');
 $Message2 = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('No','no');
 $Template = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder('Are you Sure',[$Message1,$Message2]);
-print_r($Template);
+$MessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('this is a confirm template', $Template);
+
+print_r($MessageBuilder);
