223b9f8e0c953113a1588ee0f4402f42eaa66846
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-23 10:27:12 +0700

    no message

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php
-//use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-//use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
-//use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
 
-$Message[] = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('Yes','yes');
-$Message[] = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('No','no');
-$Template = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder('Are you Sure??',$Message);
+$path = __DIR__ . '/vendor/autoload.php';
+require_once $path;
 
-$MessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('this is a confirm template.', $Template);
+$Message[] = new MessageTemplateActionBuilder('Yes','yes');
+$Message[] = new MessageTemplateActionBuilder('No','no');
+$Template = new ConfirmTemplateBuilder('Are you Sure??',$Message);
+
+$MessageBuilder = new TemplateMessageBuilder('this is a confirm template.', $Template);
 
 print_r($MessageBuilder);
