cbfa5cf9fbe8570f7069f7748c6c3ba2628b6776
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-23 10:14:48 +0700

    no message

--- bot.php
+++ bot.php
@@ -1,12 +1,27 @@
 <?php
+use LINE\LINEBot;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot\MessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
+use LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use LINE\LINEBot\MessageBuilder\LocationMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
 
 $path = __DIR__ . '/vendor/autoload.php';
 require_once $path;
 
-$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=');
-$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '515995d49d4801e7c580b8c914709b35']);
+$httpClient = new CurlHTTPClient('wzwpbz9tZWCSPDrTFYf+APzByZ3jnlV259OV13WiCcsBXMftEVvi/OzVdEy8C31CYj4iA6GdPwQ5QCBnrJPKTNC4IcxZlr4bJwIVRAPd1FlWnDG8ThGjHWY4ZIOD1V/DhshZVuUJUv+YfDrLgh6xtgdB04t89/1O/w1cDnyilFU=');
+$bot = new LINEBot($httpClient, ['channelSecret' => '515995d49d4801e7c580b8c914709b35']);
 $mid = 'Ubb0233685f6c43ad7af9f72476d67f16';
 
 $postdata = file_get_contents("php://input");
@@ -25,41 +40,41 @@ if(!is_null($events['events'])) {
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}
 			if(strpos($text, 'image') !== false){
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder('https://upload.wikimedia.org/wikipedia/commons/b/b4/JPEG_example_JPG_RIP_100.jpg','https://upload.wikimedia.org/wikipedia/en/6/6d/Pullinger-150x150.jpg');
+				$MessageBuilder = new ImageMessageBuilder('https://upload.wikimedia.org/wikipedia/commons/b/b4/JPEG_example_JPG_RIP_100.jpg','https://upload.wikimedia.org/wikipedia/en/6/6d/Pullinger-150x150.jpg');
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}
 			if(strpos($text, 'video') !== false){
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\VideoMessageBuilder('https://example.com/original.mp4','https://upload.wikimedia.org/wikipedia/commons/a/ac/Large_format_camera_lens.jpg');
+				$MessageBuilder = new VideoMessageBuilder('https://example.com/original.mp4','https://upload.wikimedia.org/wikipedia/commons/a/ac/Large_format_camera_lens.jpg');
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}
 			if(strpos($text, 'audio') !== false){
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\AudioMessageBuilder('https://example.com/original.m4a',240000);
+				$MessageBuilder = new AudioMessageBuilder('https://example.com/original.m4a',240000);
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}
 			if(strpos($text, 'location') !== false){
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\LocationMessageBuilder('my location','〒150-0002 東京都渋谷区渋谷２丁目２１−１',35.65910807942215,139.70372892916203);
+				$MessageBuilder = new LocationMessageBuilder('my location','〒150-0002 東京都渋谷区渋谷２丁目２１−１',35.65910807942215,139.70372892916203);
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}
 			if(strpos($text, 'sticker') !== false){
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1,1);
+				$MessageBuilder = new StickerMessageBuilder(1,1);
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}
 			if(strpos($text, 'tem') !== false){
-				$Message[] = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('Yes','yes');
-				$Message[] = new \LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder('No','no');
-				$Template = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder('Are you Sure??',$Message);
+				$Message[] = new MessageTemplateActionBuilder('Yes','yes');
+				$Message[] = new MessageTemplateActionBuilder('No','no');
+				$Template = new ConfirmTemplateBuilder('Are you Sure??',$Message);
 				
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder('this is a confirm template.', $Template);
+				$MessageBuilder = new TemplateMessageBuilder('this is a confirm template.', $Template);
 				$response = $bot->pushMessage($mid, $MessageBuilder);				
 			}
 			if(strpos($text, 'map') !== false){
-				$AreaUri = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(0,0,520,1040);
-				$AreaMessage = new \LINE\LINEBot\ImagemapActionBuilder\AreaBuilder(520,0,520,1040);
-				$Action[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder('https://en.wikipedia.org/wiki/Main_Page/',$AreaUri);
-				$Action[] = new \LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder('hello',$AreaMessage);
-				$BaseSize = new \LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder(1040,1040);
+				$AreaUri = new AreaBuilder(0,0,520,1040);
+				$AreaMessage = new AreaBuilder(520,0,520,1040);
+				$Action[] = new ImagemapUriActionBuilder('https://en.wikipedia.org/wiki/Main_Page/',$AreaUri);
+				$Action[] = new ImagemapMessageActionBuilder('hello',$AreaMessage);
+				$BaseSize = new BaseSizeBuilder(1040,1040);
 				
-				$MessageBuilder = new \LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder('https://upload.wikimedia.org/wikipedia/commons/a/ac/Large_format_camera_lens.jpg', 'ImageMap',$BaseSize,$Action);
+				$MessageBuilder = new ImagemapMessageBuilder('https://upload.wikimedia.org/wikipedia/commons/a/ac/Large_format_camera_lens.jpg', 'ImageMap',$BaseSize,$Action);
 				$response = $bot->pushMessage($mid, $MessageBuilder);
 			}
 		}
