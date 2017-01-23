88b0a486d91784597e3a2ff3bb71d5ff524ece5e
AommySureerat <sureerat.aom1504@gmail.com>
2017-01-23 14:03:38 +0700

    no message

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
-use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
-use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
-use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+
+use LINE\LINEBot\MessageBuilder\ImagemapMessageBuilder;
+use LINE\LINEBot\MessageBuilder\Imagemap\BaseSizeBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapMessageActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\ImagemapUriActionBuilder;
+use LINE\LINEBot\ImagemapActionBuilder\AreaBuilder;
 
 $path = __DIR__ . '/vendor/autoload.php';
 require_once $path;
 
-$Message[] = new MessageTemplateActionBuilder('Yes','yes');
-$Message[] = new MessageTemplateActionBuilder('No','no');
-$Template = new ConfirmTemplateBuilder('Are you Sure??',$Message);
+$AreaUri = new AreaBuilder(0,0,520,1040);
+$AreaMessage = new AreaBuilder(520,0,520,1040);
+$Action[] = new ImagemapUriActionBuilder('https://en.wikipedia.org/wiki/Main_Page/',$AreaUri);
+$Action[] = new ImagemapMessageActionBuilder('hello',$AreaMessage);
+$BaseSize = new BaseSizeBuilder(1040,1040);
 
-$MessageBuilder = new TemplateMessageBuilder('this is a confirm template.', $Template);
+$MessageBuilder = new ImagemapMessageBuilder('https://upload.wikimedia.org/wikipedia/commons/a/ac/Large_format_camera_lens.jpg', 'ImageMap',$BaseSize,$Action);
 
 print_r($MessageBuilder);
